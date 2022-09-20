67fde7eab6d11b459c8e114463088ec6d03f4cfa
Inzamam <inzamam@credencesoft.in>
2022-09-20 11:02:56 +0530

    Added new project Google-Keep-Clone

--- src/assets/ProjectsArray/ProjectsArray.js
+++ src/assets/ProjectsArray/ProjectsArray.js
@@ -1,16 +1,17 @@
 import youtubeClone from '../images/youtube-clone.png';
 import realEstateApp from '../images/real-Estate-app.png';
 import cryptoApp from '../images/crypto-app.png';
+import GoogleKeepClone from '../images/google-keep-clone.png';
 
 
 export const ProjectsArray = [
     {
         row: [
             {
-                name: 'Cryptocurrency App',
-                desc: 'Crytocurrency App which shows the current and previous prices along with the latest news about cryptocurrency.',
-                link: 'https://crypto-app-innzz.vercel.app',
-                image: cryptoApp
+                name: 'Google-Keep-clone',
+                desc: 'Build by using M.E.R.N Stack, It has functionalities like real Google keep. Users can Create, Update, Delete Notes. Also it has some additional functionalities like archive notes or pin notes and deleted notes will get in to trash then you can permanently delete it from there. ',
+                link: 'https://google-keep-clone-woad.vercel.app/',
+                image: GoogleKeepClone
             },
             {
                 name: 'Real Estate App',
@@ -27,6 +28,12 @@ export const ProjectsArray = [
                 desc: 'Build and Deployed a Modern YouTube Clone Application in React JS with Material UI 5',
                 link: 'https://bright-cendol-6ad010.netlify.app/',
                 image: youtubeClone
+            },
+            {
+                name: 'Cryptocurrency App',
+                desc: 'Crytocurrency App which shows the current and previous prices along with the latest news about cryptocurrency.',
+                link: 'https://crypto-app-innzz.vercel.app',
+                image: cryptoApp
             },
         ]
     }
